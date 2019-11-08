e0ac543e98cb712c0e4bac72983febbfc71cfceb
Stanley Heryanto <stanleyheryanto@Gmail.com>
2019-11-08 20:31:54 +0700

    Fresh project

---
+++ web/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:kelarin/splash.dart';
+
+void main() => runApp(Kelarin());
+
+class Kelarin extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kelarin',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Splash(),
+    );
+  }
+}
